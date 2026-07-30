0eafa9c7c73970ec3824a9696f5c094261513a77
Growthen <arielandrerevilla@gmail.com>
2026-07-29 20:37:44 -0500

    feat(config): add JPA auditing config

--- src/main/java/dev/growthen/api/config/jpa/JpaConfig.java
+++ src/main/java/dev/growthen/api/config/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package dev.growthen.apilibreria.config.jpa;
+package dev.growthen.api.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
